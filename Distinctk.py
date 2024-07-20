86f1de30550ecea9a0c8fc09542df4347abbaef2
Sanjanah8 <139737652+Sanjanah8@users.noreply.github.com>
2024-07-20 17:07:44 +0530

    Distinctk.py

--- Distinctk.py
+++ Distinctk.py
@@ -1,3 +1,12 @@
+#You wish to help Ashish, who possesses a collection of N strings, some of which may be duplicated, and has been assigned the task of finding the kth unique string.
+If the number of unique strings is less than k, he needs to display an empty string. Considering you are Ashish's best friend can you assist him with this challenge?
+Input Format
+The first line contains an integer N denoting the number of strings.
+The next N lines contain strings.
+The next line contains an integer k.
+Output Format
+The output contains the kth distinct string. If there are less than k unique string display an empty string.#
+
 def kth_unique_string(strings, k):
     # Step 1: Count occurrences of each string
     count = {}
